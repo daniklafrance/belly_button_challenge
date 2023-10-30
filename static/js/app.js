9cf39b6763f41672209fd0a4a30d6de70883c7ed
daniklafrance <daniklafrance@hotmail.com>
2023-10-30 17:23:45 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,25 @@
-const bellyButtonData = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+//data from URL in a variable
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Fetch the JSON data and console log it
-d3.json(bellyButtonData).then(function(data) {
+//JSON found and logged
+d3.json(url).then(function(data) {
     console.log(data);
   });
+
+function init() {
+    
+    let dropDown = d3.select("#selDataset");
+    d3.json(url).then((data) => {
+
+        let names = data.names;
+
+        names.forEach((id) => {            
+            dropDown.append("option")
+            .text(id)
+            .property("value", id);
+        });
+
+    });
+};
+
+init();
