4e6f5e8bd418237c2b977acc62cbb7d2b286ec5f
daniklafrance <daniklafrance@hotmail.com>
2023-10-30 11:55:55 -0400

    start

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+const bellyButtonData = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(bellyButtonData).then(function(data) {
+    console.log(data);
+  });
