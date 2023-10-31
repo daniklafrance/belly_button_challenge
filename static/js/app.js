e7d67aad21227e5fa091ab184ff93a0502fa689f
daniklafrance <daniklafrance@hotmail.com>
2023-10-31 11:49:29 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,35 @@
-//data from URL in a variable
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+//set up page with default plots
+function defaultPlots(selectedPatient) {
+  d3.json("samples.json").then((data) => {
+    let plotData = data.samples;
+    let patient = plotData.filter(
+      (samplePatient) => samplePatient.id == selectedPatient
+    )[0];
 
-//JSON found and logged
-d3.json(url).then(function(data) {
-    console.log(data);
+    console.log(patient);
+    let ids = patient.otu_ids;
+    let labels = patient.otu_labels;
+    let values = patient.sample_values;
+
+//bar chart
+    let trace1 = {
+      x: values.slice(0, 10).reverse(),
+      y: ids.slice(0, 10).map((otuID) => 'OTU ${otuID}').reverse(),
+      text: labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h"
+    };
+
+    let bardata = [trace1];
+
+    let layout = {
+      title: 'title',
+      xaxis: {autorange: true},
+      yaxis: {autorange: true},
+    }
+
+    Plotly.newPlot("bar", bardata, layout);
+
   });
+}
+
