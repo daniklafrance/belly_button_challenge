86a871ebb2a5d792b976edc98c79f961865771e4
daniklafrance <daniklafrance@hotmail.com>
2023-10-31 12:52:59 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
 //set up default plots, data variables created
 function defaultPlots(selectedPatient) {
-  d3.json("samples.json").then((data) => {
+  d3.json(url).then((data) => {
     let plotData = data.samples;
     let patient = plotData.filter(
       (samplePatient) => samplePatient.id == selectedPatient
@@ -57,27 +59,27 @@ function defaultPlots(selectedPatient) {
 
 //demographic info panel
 function demoPanel(selectedPatient) {
-  d3.json("samples.json").then((data) => {
+  d3.json(url).then((data) => {
     let demoData = data.metadata;
     let patient = demoData.filter(
       (samplePatient) => samplePatient.id == selectedPatient
     )[0];
     let demoPanelBox = d3.select("#sample-metadata");
     demoPanelBox.html("");
-    Object.entries(patient).forEach(([key, value]) => {
+    Object.entries(patient).forEach(([key,value]) => {
       demoPanelBox.append("h5").text('${key}: ${value}');
     });
   });
 }
 
 function init() {
-  d3.json("samples.json").then(function (data) {
-    console.log("samples.json:", data);
+  d3.json(url).then(function (data) {
+    console.log('url:', data);
 
     let dropDown = d3.select('#selDataset');
 
     data.names.forEach((name) => {
-      dropDown.append('option').text(name).property('value', name;)
+      dropDown.append('option').text(name).property('value', name);
     });
 
     const firstPatient = data.names[0];
