4bb9cd04b8ca1ff44a0a11dff859b7e33692cf63
daniklafrance <daniklafrance@hotmail.com>
2023-11-01 14:02:01 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,22 @@
+//variable for the data website
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+//use d3 to retrieve the json data
 d3.json(url).then(function(data){
   console.log(data);
 });
 
 //demographic info panel
+//new function for demo panel
 function demoPanel(newPatient) {
   d3.json(url).then((data) => {
 
+//find the metadata, filter to match the selection and assign variable to the selection
     let demoData = data.metadata;
     let patient = demoData.filter(item => item.id == newPatient);
     let patientData = patient[0]
 
+//use d3 to make changes to html code
     d3.select("#sample-metadata").html("");
     Object.entries(patientData).forEach(([key,value]) => {
       d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
@@ -20,17 +25,21 @@ function demoPanel(newPatient) {
 };
 
 //bar chart
+//new function for bar chart
 function barPlot(newPatient) {
   d3.json(url).then((data) => {
 
+//find samples, filter and assign variable to the selection
     let plotData = data.samples;   
     let patient = plotData.filter(item => item.id == newPatient);    
     let patientData = patient[0]
 
+//variables for specific data from selection    
     let ids = patientData.otu_ids;
     let labels = patientData.otu_labels;
     let values = patientData.sample_values;
 
+//create bar chart
     let barChart = {
       x: values.slice(0, 10).reverse(),
       y: ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
@@ -50,17 +59,21 @@ function barPlot(newPatient) {
 };
 
 //bubble chart
+//new function for bubble chart
 function bubblePlot(newPatient) {
   d3.json(url).then((data) => {
 
+//find samples, filter and assign variable to the selection
     let bubbleplotData = data.samples;   
     let patient = bubbleplotData.filter(item => item.id == newPatient);    
     let patientData = patient[0]
 
+//variables for specific data from selection   
     let ids = patientData.otu_ids;
     let labels = patientData.otu_labels;
     let values = patientData.sample_values;
 
+//create bubble chart
     let bubbleChart = {
       x: ids,
       y: values,
@@ -69,7 +82,6 @@ function bubblePlot(newPatient) {
       marker: {
         size: values,
         color: ids,
-        showscale: true,
       }
     };
 
@@ -83,22 +95,27 @@ function bubblePlot(newPatient) {
   });
 };
 
+//new function to input the names in html code
 function init() {
 
+//select the dropdown code  
   let dropDown = d3.select("#selDataset");
 
+//find data
   d3.json(url).then((data) => {
 
+//find selections(names)
     let dropDownSelect = data.names;
-    
-    dropDownSelect.forEach((id) => {
-      console.log(id);
 
-      dropDown.append("option")
-      .text(id)
-      .property("value", id);
+//log names into console
+    dropDownSelect.forEach((name) => {
+      console.log(name);
+
+//input the selections into the html code
+      dropDown.append("option").text(name).property("value", name);
     });
 
+//first selection shown
     let firstPatient = dropDownSelect[0];
 
     demoPanel(firstPatient);
@@ -108,10 +125,11 @@ function init() {
   });
 };
 
-function refresh(patient) {
-  barPlot(patient);
-  bubblePlot(patient);
-  demoPanel(patient);
+//function to change data shown when selection is changed
+function optionChanged(value) {
+  barPlot(value);
+  bubblePlot(value);
+  demoPanel(value);
 };
 
 init();
